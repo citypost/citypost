2d6d0561cd18876b5e16e3cabec34ce9b1a600db
pihh.rocks@gmail.com <pihh.rocks@gmail.com>
2015-12-17 12:09:03 +0000

    added class autoloader, started MVC router + clean urls

--- account/index.php
+++ account/index.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: filipe_2
- * Date: 12/17/2015
- * Time: 9:46 AM
- */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+define('VIEW_PATH', ROOT.DS."views");
+
+# Getting the urls for the MVC
+$url = $_SERVER["REQUEST_URI"];
+$method = $_SERVER["REQUEST_METHOD"];
+
+# including the required filesystem
+require_once(ROOT.DS."account".DS."config".DS."init.php");
+
+echo ROOT;
