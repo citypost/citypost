947d9aef4196b66b7bc93ffb5abad3779cdd423d
pihh.rocks@gmail.com <pihh.rocks@gmail.com>
2015-12-17 13:43:45 +0000

    added footer

--- account/views/footer.php
+++ account/views/footer.php
@@ -1,7 +1,90 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: filipe_2
- * Date: 12/17/2015
- * Time: 1:15 PM
- */
+    <div id="footer-container" class="">
+
+        <div class="container">
+
+            <div class="row rowFirst">
+
+                <div class="col-lg-2 col-lg-offset-1 col-md-2 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 footer" style="padding-left:0px;">
+
+                    <h5>Serviços Postais</h5>
+                    <ul class="nav footer-links">
+                        <li><a href="enviar-correio.html" class="navbar-link">Enviar Correio</a></li>
+                        <li><a href="correio-nacional.html">Correio Nacional</a></li>
+                        <li><a href="correio-internacional.html">Correio Internacional</a></li>
+                        <li><a href="dm-post.html">DM Post</a></li>
+                        <li><a href="ipost.html">iPost</a></li>
+                        <li><a href="dm-inteligente.html">DM Inteligente</a></li>
+                        <li><a href="http://parcel.citypostsystem.com/default.aspx">Encomendas</a></li>
+                        <li><a href="producao-postal.html">Produção Postal</a></li>
+                        <li><a href="http://www2.citypost.pt/ClientArea/Account/Login.aspx">Monitorize a sua Entrega!</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-lg-offset-0 col-md-2 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 footer2">
+                    <h5>Sobre a Empresa</h5>
+                    <ul class="nav footer-links">
+                        <li><a href="sobre-a-citypost.html">Sobre a CityPOST</a></li>
+                        <li><a href="oportunidades-de-emprego.html">Oportunidades de emprego</a></li>
+                        <li><a href="politica-de-privacidade.html">Política de Privacidade</a></li>
+                        <li><a href="politica-de-cookies.html">Política de <i>cookies</i></a></li>
+                        <li><a href="seguranca-web.html">Segurança Web</a></li>
+                        <li><a href="mapa-do-site.html">Mapa do Site</a></li>
+                        <li><a href="conteudos-multimedia.html">Conteúdos Multimédia</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-lg-offset-0 col-md-2 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 footer2">
+                    <h5>Serviço ao Cliente</h5>
+                    <ul class="nav footer-links">
+                        <li><a href="#">Contactos</a></li>
+                        <li><a href="perguntas-frequentes-sobre-os-servicos-postais.html">Perguntas frequentes sobre os Serviços Postais</a></li>
+                        <li><a href="perguntas-frequentes-sobre-encomendas.html">Perguntas frequentes sobre Encomendas</a></li>
+                        <li><a href="porque-utilizar-os-nossos-servicos.html">Porquê utilizar os nosso serviços</a></li>
+                        <li><a href="procedimento-em-caso-de-reclamacao.html">Procedimento em caso de Reclamação</a></li>
+                        <li><a href="termos-e-condicoes-para-servicos-postais.html">Termos e Condições Para Serviços Postais</a></li>
+                        <li><a href="termos-e-condicoes-para-encomendas.html">Termos e Condições Para Encomendas</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-lg-offset-0 col-md-2 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 footer2 lastFooter">
+                    <h5 class="not-uppercase">CityPOST SA</h5>
+                    <ul class="nav footer-links">
+                        <li>
+                            <span>R. Alfredo da Silva 8A</span>
+                            <br>
+                            <span>Edifício Stern, 1º A</span>
+                            <br>
+                            <span>2610-016 Alfragide, Portugal</span>
+                        </li>
+                        <li>&nbsp;</li>
+                        <li><span><span class="glyphicon glyphicon-earphone"></span> 21 470 44 44</span>
+                        </li>
+                        <li><a href="mailto:info@citypost.pt"><span class="glyphicon glyphicon-envelope"></span> info@citypost.pt</a></li>
+                        <li>&nbsp;</li>
+                        <li>
+                            <ul id="social-cell">
+                                <li>
+                                    <a target="_blank" href="https://www.facebook.com/CityPOST.pt"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCIyxvr0fpRC7hbs_jq_ne-g"><i class="fa fa-youtube"></i></a>
+                                </li>
+                                <li>
+                                    <a target="_blank" href="https://plus.google.com/115538219219061415820/about"><i class="fa fa-google-plus"></i></a>
+                                </li>
+                                <li>
+                                    <a target="_blank" href="https://twitter.com/CitypostPt"><i class="fa fa-twitter"></i></a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- end of uniqe row  -->
+
+        </div>
+
+    </div>
+    <!-- end of footer-container  -->
+
+    <div id="footer-rodape-container" class="direitos">
+Direitos autorais © 2015 CityPOST | Todos os direitos reservados
+</div>
--- account/views/index.php
+++ account/views/index.php
@@ -4,13 +4,13 @@
  * Includes: Header, Footer, base template, SEO
  */
 ?>
-<head>
+<head xmlns="http://www.w3.org/1999/html">
 
 </head>
 <body>
 <?php include_once(VIEW_PATH . DS . "nav.php"); ?>
 
-
+</div></div></div>
 <?php include_once(VIEW_PATH . DS . "footer.php"); ?>
 </body>
 
